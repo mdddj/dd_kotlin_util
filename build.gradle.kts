bb3878bd255fce62ad1e3533be480b34a487b4f3
梁典典 <413153189@qq.com>
2024-05-25 11:26:16 +0800

    release1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.util.*
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.3"
     id("org.jetbrains.changelog") version "2.2.0"
 }
 
